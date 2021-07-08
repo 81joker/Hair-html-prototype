d1475cd474a10ae113fe55d1544bddaf3e95f9d9
Nehad <nehad.al.timimi@gmail.com>
2021-07-08 08:04:24 +0200

    frist commit

---
+++ js/main.js
@@ -0,0 +1,13 @@
+var myCarousel = document.querySelector("#myCarousel");
+//var carouselz = new bootstrap.Carousel(myCarousel);
+
+
+
+
+
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+})
